9b258b7c6cd6781cb92cc62d63063949e0ff89d8
cchi852 <chris.chiem01@gmail.com>
2024-10-04 00:54:10 +1300

    images

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,9 @@
 """App entry point."""
+import os
 from podcast import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=5000, threaded=False)
+    port = int(os.environ.get("PORT", 5000))  # Use the port defined by Render, fallback to 5000 locally
+    app.run(host="0.0.0.0", port=port, threaded=False)
