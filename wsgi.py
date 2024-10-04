4dd57a50fdbf6289d42ef162e05945f71fa4d5e1
cchi852 <chris.chiem01@gmail.com>
2024-10-04 23:22:17 +1300

    fixed port

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,4 @@ from podcast import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=False, debug=True)
+    app.run(host='0.0.0.0', port=8080, threaded=False, debug=True)
